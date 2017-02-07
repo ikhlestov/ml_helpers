5a000fdfaf630de10b53fb6b35aa254249828b68
Illarion <khlyestovillarion@gmail.com>
2017-02-07 18:15:54 +0200

    Add cache for MNIST strokes data provider

--- ml_helpers/data_providers/mniststrokes.py
+++ ml_helpers/data_providers/mniststrokes.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+import pickle
 
 import numpy as np
 from tqdm import tqdm
@@ -59,7 +60,7 @@ class MNISTStrokesDataProvider(DataProvider):
     n_classes = 10
 
     def __init__(self, one_hot=True, save_path=None, validation_split=None,
-                 shuffle=True, verbose=True):
+                 shuffle=True, verbose=True, use_cache=True):
         """
         Args:
             one_hot: `bool`, return lasels one hot encoded
@@ -69,52 +70,24 @@ class MNISTStrokesDataProvider(DataProvider):
                 float: chunk of `train set` will be marked as `validation set`.
             shuffle: `bool`, should shuffle data or not
             verbose: `bool`, should report some pregress
+            use_cache: `bool`, try to pickle/unpicke  processed data
         """
+        self.use_cache = True
         self._save_path = None
+        self._verbose = verbose
+        self._one_hot = one_hot
         download_data_url(self.data_url, self.save_path, verbose=verbose)
-        sub_save_path = os.path.join(self.save_path, 'sequences')
         for dataset in ['test', 'train']:
-            labels_file = os.path.join(sub_save_path, '%slabels.txt' % dataset)
-            with open(labels_file, 'r') as f:
-                labels = f.readlines()
-            # labels, one_hot=False
-            labels = [int(l) for l in labels]
-            dataset_points = []
-            dataset_inputs = []
-            dataset_targets = []
-            dataset_labels = np.array(labels)
-            if one_hot:
-                dataset_labels = self.labels_to_one_hot(dataset_labels)
-            if verbose:
-                description = 'Read %s dataset from text files' % dataset
-                labels_range = enumerate(tqdm(labels, desc=description))
+            if self.use_cache:
+                success, data = self._load_cached_data(dataset)
             else:
-                labels_range = enumerate(labels)
-            for label_idx, label in labels_range:
-                part_f_name = '%simg-%d' % (dataset, label_idx)
-                points_file = os.path.join(
-                    sub_save_path, '%s-points.txt' % part_f_name)
-                inputs_data_file = os.path.join(
-                    sub_save_path, '%s-inputdata.txt' % part_f_name)
-                targets_data_file = os.path.join(
-                    sub_save_path, '%s-targetdata.txt' % part_f_name)
-
-                label_points = []
-                with open(points_file, 'r') as f:
-                    for line in f.readlines():
-                        try:
-                            point = [int(i.strip()) for i in line.split(',')]
-                            label_points.append(point)
-                        except ValueError:
-                            continue
-                label_points = np.array(label_points)
-                dataset_points.append(label_points)
-
-                inputs = np.loadtxt(inputs_data_file, dtype='int')
-                dataset_inputs.append(inputs)
-
-                targets = np.loadtxt(targets_data_file, dtype='int')
-                dataset_targets.append(targets)
+                success = False
+            if not success:
+                data = self._get_new_data(dataset)
+            if self.use_cache and not success:
+                self._save_cached_data(dataset, data)
+            (dataset_points, dataset_inputs,
+             dataset_targets, dataset_labels) = data
 
             if dataset == 'test':
                 self.test = MNISTStrokesDataSet(
@@ -146,6 +119,76 @@ class MNISTStrokesDataProvider(DataProvider):
                     dataset_targets,
                     dataset_labels,
                     shuffle=shuffle)
+
+    def _get_new_data(self, dataset):
+        sub_save_path = os.path.join(self.save_path, 'sequences')
+        labels_file = os.path.join(sub_save_path, '%slabels.txt' % dataset)
+        with open(labels_file, 'r') as f:
+            labels = f.readlines()
+        # labels, one_hot=False
+        labels = [int(l) for l in labels]
+        dataset_points = []
+        dataset_inputs = []
+        dataset_targets = []
+        dataset_labels = np.array(labels)
+        if self._one_hot:
+            dataset_labels = self.labels_to_one_hot(dataset_labels)
+        if self._verbose:
+            description = 'Read %s dataset from text files' % dataset
+            labels_range = enumerate(tqdm(labels, desc=description))
+        else:
+            labels_range = enumerate(labels)
+        for label_idx, label in labels_range:
+            part_f_name = '%simg-%d' % (dataset, label_idx)
+            points_file = os.path.join(
+                sub_save_path, '%s-points.txt' % part_f_name)
+            inputs_data_file = os.path.join(
+                sub_save_path, '%s-inputdata.txt' % part_f_name)
+            targets_data_file = os.path.join(
+                sub_save_path, '%s-targetdata.txt' % part_f_name)
+
+            label_points = []
+            with open(points_file, 'r') as f:
+                for line in f.readlines():
+                    try:
+                        point = [int(i.strip()) for i in line.split(',')]
+                        label_points.append(point)
+                    except ValueError:
+                        continue
+            label_points = np.array(label_points)
+            dataset_points.append(label_points)
+
+            inputs = np.loadtxt(inputs_data_file, dtype='int')
+            dataset_inputs.append(inputs)
+
+            targets = np.loadtxt(targets_data_file, dtype='int')
+            dataset_targets.append(targets)
+        return dataset_points, dataset_inputs, dataset_targets, dataset_labels
+
+    def _get_pickle_filename(self, dataset):
+        cache_dir = os.path.join(
+            tempfile.gettempdir(), 'mnist_strokes', 'cache')
+        os.makedirs(cache_dir, exist_ok=True)
+        pickle_filename = os.path.join(cache_dir, '%s_data.pkl' % dataset)
+        return pickle_filename
+
+    def _load_cached_data(self, dataset):
+        pickle_filename = self._get_pickle_filename(dataset)
+        try:
+            with open(pickle_filename, 'rb') as f:
+                data = pickle.load(f)
+                success = True
+        except Exception as e:
+            if self._verbose:
+                print("Failed to load cache with exception: %s" % str(e))
+            success = False
+            data = None
+        return success, data
+
+    def _save_cached_data(self, dataset, data_to_save):
+        pickle_filename = self._get_pickle_filename(dataset)
+        with open(pickle_filename, 'wb') as f:
+            pickle.dump(data_to_save, f)
 
     @property
     def save_path(self):
